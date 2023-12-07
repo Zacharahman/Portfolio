4984d6cdc7b9a12878338c8f3836e3b7b4fe1194
Zach Abdulrahman <Zacharahman365@yahoo.com>
2023-12-07 14:32:47 -0500

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,31 +6,37 @@ function Contact() {
       <div className="flex flex-col mb-10 mx-auto">
          <div className="flex justify-center items-center">
             <form
-               action="https://getform.io/f/4dc0c49c-a9a0-475a-9483-e9054d3ff388"
+               action="https://getform.io/f/4926a01b-0c9c-414d-a8f0-b58bd078ea3f"
                method="POST"
                className="flex flex-col w-full md:w-7/12"
             >
                <Title>Contact</Title>
+               <label htmlFor="name">Name</label>
                <input
                   type="text"
+                  id="name"
                   name="name"
                   placeholder="Name"
                   className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
                />
+               <label htmlFor="email">Email</label>
                <input
                   type="text"
+                  id="email"
                   name="email"
                   placeholder="Email"
                   className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
                />
+               <label htmlFor="message">Message</label>
                <textarea
+                  id="message"
                   name="message"
                   placeholder="Message"
                   rows="10"
                   className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <button
-                  type="button"
+                  type="submit" {/* Change type to submit */}
                   className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                >
                   Work With Me
