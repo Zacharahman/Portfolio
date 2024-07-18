ef3329a0b511bd623f2af5f5391a685a2aa67a1a
Zach Abdulrahman <Zacharahman365@yahoo.com>
2024-07-18 16:25:43 -0400

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Zλch Abdulrahman</h1>
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Zach Abdulrahman</h1>
          <p className="text-base md:text-xl mb-3 font-medium">CS + Math</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
             Hi, I'm Zach! I am a Software Engineer and Mathematician located in Columbia, SC. I am dedicated to full-stack development with specific interests in machine learning, computational neuroscience and applied mathematics. I currently attend the University of South Carolina pursuing a Bachelor of Science in Computer Science as well as a Bachelor of Science in Mathematics with a minor in Data Science. Some hobbies of mine include hiking, cooking and playing the double bass in my University Orchestra!
@@ -15,7 +15,16 @@ function Intro() {
                className="text-red-700 hover:underline underline-offset-2 decoration-2 decoration-red-700"
                rel="noreferrer noopener"
             >
-              <u> Here </u> 
+              <u> Tutoring </u> 
+            </a>{''}
+             Check out my math blog to stay up to date {''}
+            <a
+               href="https://zacharahman.github.io/TheMathMatrix.github.io/"
+               target="_blank"
+               className="text-red-700 hover:underline underline-offset-2 decoration-2 decoration-red-700"
+               rel="noreferrer noopener"
+            >
+              <u> Math Blog </u> 
             </a>{''}
             
          </p>
